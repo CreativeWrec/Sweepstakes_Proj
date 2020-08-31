6c6cc4a061159d637d5579ea08c4c3d2d1f9dd7b
Kevin Harvey Jr <lordty@gmail.com>
2020-08-31 14:13:22 -0500

    Added_MarketingFirm_Class

---
+++ Sweepstakes_Project/MarketingFirm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes_Project
+{
+    class MarketingFirm
+    {
+        private ISweepstakesManager _manager;
+
+        public MarketingFirm(ISweepstakesManager manager)
+        {
+
+        }
+
+        public void CreateSweepstakes()
+        {
+
+        }
+
+    }
+}
