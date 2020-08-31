c99f3f22249bd8f2c957590bb015e16fe5bbc806
Kevin Harvey Jr <lordty@gmail.com>
2020-08-31 14:45:17 -0500

    Added_SweepstakesStackManager

---
+++ Sweepstakes_Project/SweepstakesStackManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes_Project
+{
+    class SweepstakesStackManager
+    {
+        private Stack<Sweepstakes> stack;
+    }
+    public void InsertSweepstakes(Sweepstakes sweepstakes)
+    {
+
+    }
+    public Sweepstakes GetSweepstakes()
+    {
+
+    }
+}
---
+++ Sweepstakes_Project/UserInterface.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes_Project
+{
+    static interface UserInterface
+    {
+        public static string GetUserInputFor(string prompt)
+    }
+}
